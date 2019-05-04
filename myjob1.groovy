02b581e0f22a47ff54feb6c656003c4c05896ae0
seb2b <33464266+seb2b@users.noreply.github.com>
2019-05-04 17:53:24 +0200

    Update myjob1.groovy

--- myjob1.groovy
+++ myjob1.groovy
@@ -1,15 +1,15 @@
 job('My Job 1') {
     scm {
-        git('https://github.com/akasam/jenkins-demo1') {  node -> 
-            node / gitConfigName('srohaut  ')
-            node / gitConfigEmail('srohaut@estiam.com')
+        git('https://github.com/seb2b/tp-jenkins-demo1') {  node -> 
+            node / gitConfigName('salliot  ')
+            node / gitConfigEmail('seballiot@orange.fr')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('NodeJS10')
+        nodejs('NodeJS9')
     }
     steps {
         shell("npm install")
